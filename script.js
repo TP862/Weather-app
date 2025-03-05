6b3e719eb4201418ca5ea20dc78dccc10cf2997b
franc <francoisdestroo@gmail.com>
2025-03-05 21:02:48 +0200

    how tf do i read from a json file n js why is it so hard

--- script.js
+++ script.js
@@ -16,9 +16,29 @@ function showText() {
     document.getElementById("displayedText").textContent = "Hello World"
 }
 
+//takes what user puts in text box and outputs that text into a paragraph
 function getUserInput() {
-
     const cityName = document.getElementById("cityName").value;
-
     document.getElementById("result").textContent = cityName;
+}
+
+//function to set contents of the paragraph
+function setResult(input) {
+    document.getElementById("result").textContent = input;
+}
+
+//grabs the weather data from the json file
+function getJsonData() {
+    fetch('data.json')
+    .then(Response =>{ 
+    
+        if (!Response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return Response.json();
+    })
+    .then(data => document.getElementById("result").textContent = data.temp)
+    .catch(error => {
+        console.error('Error reading json file: ', error)
+    });
 }
