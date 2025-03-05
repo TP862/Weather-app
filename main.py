76837bf28ec688333e56dacb823f2dadbb95d0cf
franc <francoisdestroo@gmail.com>
2025-03-05 20:21:43 +0200

    a json file is now generated to store weather data

--- main.py
+++ main.py
@@ -3,15 +3,24 @@ import python_weather
 import asyncio
 import os
 
+import json
+
 async def getweather() -> None:
  async with python_weather.Client(unit=python_weather.METRIC) as client:
     weather = await client.get('Stellenbosch')
 
-    print(weather.location)
-    print(weather.local_population)
+    data = {
+      "temp" : weather.temperature,
+      "pressure" : weather.pressure,
+      "humidity" : weather.humidity
+    }
+
+    with open("data.json", "w") as json_file:
+      json.dump(data, json_file, indent=4)
 
 if __name__ == '__main__':
    if os.name == 'nt':
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 asyncio.run(getweather())
+
--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 const button = document.createElement('button')
 button.innerText = 'can you click me'
+
+button.id = 'mainButton'
 button.addEventListener('click', () => {
-    alert('oh, you clicked me!')
+    alert('oh, you clicked me!');
 })
 
 document.body.appendChild(button)
+
+function showAlert() {
+    alert("clicky clacky");
+}
+
+function showText() {
+    document.getElementById("displayedText").textContent = "Hello World"
+}
+
+function getUserInput() {
+
+    const cityName = document.getElementById("cityName").value;
+
+    document.getElementById("result").textContent = cityName;
+}
