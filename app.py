dbe448c702993b68d3f6c736e4e151fec52b1ed1
franc <francoisdestroo@gmail.com>
2025-03-06 20:33:59 +0200

    added a sqlite database to store weather data

--- app.py
+++ app.py
@@ -25,9 +25,29 @@ if __name__ == '__main__':
 
 asyncio.run(getweather())
 
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///weather.db'
+db = SQLAlchemy(app)
+
+class weather(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  temperature = db.Column(db.Integer, nullable=False)
+  pressure = db.Column(db.Float, nullable=False)
+  humidity = db.Column(db.Integer, nullable=False)
+  region = db.Column(db.String(100), nullable=False)
+  date_recorded = db.Column(db.DateTime, default=datetime.now)
+
+  def __repr__(self):
+    return '<Region %r>' % self.region
+  
+  with app.app_context():
+
+        db.create_all()
+
 
 @app.route('/')
 def index():
