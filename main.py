76f63ef35babf958b2a4331a788764b2953a61e4
franc <francoisdestroo@gmail.com>
2025-03-06 18:58:35 +0200

    flask is cool i tried to implement it failed and now im gonno learn from the ground up

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ import json
 
 async def getweather() -> None:
  async with python_weather.Client(unit=python_weather.METRIC) as client:
-    weather = await client.get('Stellenbosch')
+    weather = await client.get('Tokyo')
 
     data = {
       "temp" : weather.temperature,
       "pressure" : weather.pressure,
       "humidity" : weather.humidity,
-      "region" : weather.region
+      "region" : weather.region,
+      "time" : weather.datetime
     }
 
     with open("data.json", "w") as json_file:
@@ -24,4 +25,3 @@ if __name__ == '__main__':
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 asyncio.run(getweather())
-
--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-import getweather from main.py
 
 const button = document.createElement('button')
 button.innerText = 'can you click me'
@@ -7,27 +6,6 @@ button.id = 'mainButton'
 button.addEventListener('click', () => {
     alert('oh, you clicked me!');
 })
-
-document.body.appendChild(button)
-
-function showAlert() {
-    alert("clicky clacky");
-}
-
-function showText() {
-    document.getElementById("displayedText").textContent = "Hello World"
-}
-
-//takes what user puts in text box and outputs that text into a paragraph
-function getUserInput() {
-    const cityName = document.getElementById("cityName").value;
-    document.getElementById("result").textContent = cityName;
-}
-
-//function to set contents of the paragraph
-function setResult(input) {
-    document.getElementById("result").textContent = input;
-}
 
 //grabs the weather data from the json file
 function getJsonData() {
@@ -39,9 +17,8 @@ function getJsonData() {
         }
         return Response.json();
     })
-    .then(data => document.getElementById("result").textContent = data)
+    .then(data => document.getElementById("result").textContent = data.region)
     .catch(error => {
         console.error('Error reading json file: ', error)
     });
 }
-
