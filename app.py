8858ae45dac91afc63aacf688f58c914f7761d8b
franc <francoisdestroo@gmail.com>
2025-03-06 19:49:31 +0200

    index.html moved using flask and render_template to render the website

--- app.py
+++ app.py
@@ -24,3 +24,14 @@ if __name__ == '__main__':
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 asyncio.run(getweather())
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+if __name__ == "__main__":
+  app.run(debug=True)
